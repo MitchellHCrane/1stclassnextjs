64fd5060e263cc558e8e1de2753e6c35f85cfbb0
MitchellHCrane <mitchellhcrane@gmail.com>
2026-09-15 20:26:42 -0600

    update to reflect needed changes for compliance

--- src/app/terms/page.js
+++ src/app/terms/page.js
@@ -31,6 +31,12 @@ const Terms = () => {
         Reply STOP to cancel.
         <br />
         Reply HELP for help.
+        <br />
+        Message frequency varies based on activity.
+        <br />
+        Phone numbers and SMS opt-in data collected by First Class Home
+        Mortgage will not be sold or shared with third parties or affiliates
+        for marketing or promotional purposes.
       </p>
 
       <h2 className="text-xl font-bold mb-2">4. Opt-In Requirement</h2>
