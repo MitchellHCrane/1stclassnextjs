77e8675d28bb0b0d5a0db7ed8a26be817582602b
MitchellHCrane <mitchellhcrane@gmail.com>
2026-09-17 22:37:37 -0600

    form fix

--- src/app/components/SmsConsentForm.js
+++ src/app/components/SmsConsentForm.js
@@ -14,7 +14,7 @@ export default function SmsConsentForm() {
     const formData = new FormData(form);
 
     try {
-      const response = await fetch("/__forms.html", {
+      const response = await fetch("/", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: new URLSearchParams(formData).toString(),
